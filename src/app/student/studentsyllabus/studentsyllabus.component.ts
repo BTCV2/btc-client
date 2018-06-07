f7b02dd3360beda439bd35e840de30f9e7c05a77
GO-MADsolutions <gomadsolutions@gmail.com>
2018-06-07 18:37:22 +0530

    Syllabus Empty Handled.

--- src/app/student/studentsyllabus/studentsyllabus.component.ts
+++ src/app/student/studentsyllabus/studentsyllabus.component.ts
@@ -16,16 +16,24 @@ export class StudentsyllabusComponent implements OnInit {
   datarows = [];
   temprows = [];
   marksdemo = [];
+  studentCompletedSyllabus = [];
   role: string;
   standard: string;
   rollNumber: string;
   formGroup: FormGroup;
+  showSyllabusTable: boolean;
+  subjectForm: FormGroup;
   editing= {};
   constructor(private lessonservice: LessonsService, private formBuilder: FormBuilder,
               private syllabusService:SyllabusService, private route: ActivatedRoute) {
   }
 
   ngOnInit() {
+    this.showSyllabusTable = false;
+    this.subjectForm = this.formBuilder.group({
+      subject: ['']
+    })
+
     this.formGroup = this.formBuilder.group({
       completeStatus : ['']
     })
@@ -37,87 +45,128 @@ export class StudentsyllabusComponent implements OnInit {
         this.rollNumber = params['rollNumber']
       }
     )
-    const lessonParams = {
-      standard: '12',
-      subject: 'physics'
-    };
-    this.getMarks(lessonParams);
-    this.marks = [{prop: 'Lessons'}];
-    this.marksdemo = ['Lessons'];
+    this.marks = [];
+    this.marksdemo = ['Lesson'];
 
   }
 
   getMarks = (lessonParams) => {
+    let mark = [];
     this.lessonservice.getMarks(lessonParams).subscribe(
       (res:any) => {
-       const mark =  res[0].marks;
-        mark.forEach((val, key) => {
-          if( val === '1'){
-            this.marks.push({prop: 'One'});
-            this.marksdemo.push('One');
-          } else if (val === '2') {
-            this.marks.push({prop: 'Two'});
-            this.marksdemo.push('Two');
-          }
-          else if (val === '3') {
-            this.marks.push({prop: 'Three'});
-            this.marksdemo.push('Three');
-          }
-          else if (val === '5') {
-            this.marks.push({prop: 'Five'});
-            this.marksdemo.push('Five');
-          }
-          else if (val === '10') {
-            this.marks.push({prop: 'Ten'});
-            this.marksdemo.push('Ten');
-          }
-        })
+        mark =  res[0].marks;
       },
       (err) => {
-
       },
-
       () => {
-        this.getStudentSyllabusCompletion();
+        let marks = [];
+        let marksdemo = [];
+        marks.push({prop: 'Lesson'});
+        marksdemo.push('Lesson')
+        mark.forEach((val, key) => {
+          if( val === '1'){
+            marks.push({prop: 'One'});
+            marksdemo.push('One');
+          } else if (val === '2') {
+            marks.push({prop: 'Two'});
+            marksdemo.push('Two');
+          }
+          else if (val === '3') {
+            marks.push({prop: 'Three'});
+            marksdemo.push('Three');
+          }
+          else if (val === '5') {
+            marks.push({prop: 'Five'});
+            marksdemo.push('Five');
+          }
+          else if (val === '10') {
+            marks.push({prop: 'Ten'});
+            marksdemo.push('Ten');
+          } });
+        this.marks = marks;
+        this.marksdemo = marksdemo;
+        this.getLessonAndUpdate();
       }
     )
   }
 
   getStudentSyllabusCompletion = () => {
-    this.syllabusService.getStudentSyllabus(this.standard,'physics',this.rollNumber).subscribe(
-      (res) => {
+    this.syllabusService.getStudentSyllabus(this.standard,this.subjectForm.value.subject,this.rollNumber).subscribe(
+      (res:any) => {
+        this.studentCompletedSyllabus = res.lessonName;
       console.log(res);
       },
       (err) => {
-
+        this.studentCompletedSyllabus = [];
+        this.getLessonAndUpdate();
       },
       () => {
-        this.getLessonAndUpdate();
+
       }
     );
   }
 
   getLessonAndUpdate = () => {
+    console.log('this.subjectForm.value.subject',this.subjectForm.value.subject);
     const lessonParams = {
-      standard: '12',
-      subject: 'physics'
+      standard: this.standard,
+      subject: this.subjectForm.value.subject
     };
     this.lessonservice.getLessons(lessonParams).subscribe(data => {
-        data.forEach((value, index) => {
-          this.temprows.push({
-            'Lessons': value.lessonName,
-            'One': 'INCOMPLETE',
-            'Two': 'INCOMPLETE',
-            'Three': 'INCOMPLETE',
-            'Five': 'INCOMPLETE'
-          });
+      if( this.studentCompletedSyllabus.length < 1){
+      data.forEach((value, index) => {
+        let syllabusTemp:any = {};
+        this.marks.forEach((v) => {
+          if( v.prop === 'Lesson') {
+            syllabusTemp.Lesson = value.lessonName;
+          } else{
+            syllabusTemp[v.prop] =  'INCOMPLETE';
+          }
         });
+        this.temprows.push(syllabusTemp);
+        this.datarows = this.temprows;
+        console.log(' this.datarows completed', this.datarows);
+        if ( this.role === 'admin' ) {
+          this.marksdemo.push('edit');
+        }
+            /*this.temprows.push({
+              'Lesson': value.lessonName,
+              'One': 'INCOMPLETE',
+              'Two': 'INCOMPLETE',
+              'Three': 'INCOMPLETE',
+              'Five': 'INCOMPLETE'
+            });*/
+
+        });
+      } else {
+        data.forEach((value) => {
+            this.studentCompletedSyllabus.forEach((val) => {
+              if (value.lessonName === val.lessonName) {
+                let syllabusTemp:any = {};
+                  this.marks.forEach((v) => {
+                    if( v === 'Lesson') {
+                      syllabusTemp.Lesson = v
+                    } else{
+                      syllabusTemp[v] = val.status === 'COMPLETE' ? 'COMPLETE' : 'INCOMPLETE'
+                    }
+                });
+                this.temprows.push(syllabusTemp);
+              }
+            })
+        });
+        this.datarows = this.temprows;
+        console.log(' this.datarows completed', this.datarows);
+        if ( this.role === 'admin' ) {
+          this.marksdemo.push('edit');
+        }
+      }
       },
       err => {
         console.log(err);
       },
       () => {
         this.datarows = this.temprows;
+        console.log(' this.datarows completed', this.datarows);
         if ( this.role === 'admin' ) {
           this.marksdemo.push('edit');
         }
@@ -131,5 +180,14 @@ export class StudentsyllabusComponent implements OnInit {
     this.datarows = [...this.datarows];
     console.log('this.datarows',this.datarows);
     console.log('UPDATED!', this.datarows[rowIndex][cell]);
+  }
+
+  toggleSyllabusTable = () => {
+    const lessonParams = {
+      standard: this.standard,
+      subject: this.subjectForm.value.subject
+    };
+    this.getMarks(lessonParams);
+    this.showSyllabusTable = true;
   }
 }
