ece84556caeaeb1456ac8a3295a385ef98a6a0a8
GO-MADsolutions <gomadsolutions@gmail.com>
2018-06-13 21:57:29 +0530

    Dist folder removed

--- out-tsc/app/app/constants.js
+++
@@ -1,8 +0,0 @@
-var Constants = (function () {
-    function Constants() {
-        this.base_server_url = 'https://agile-lake-26447.herokuapp.com';
-    }
-    return Constants;
-}());
-export { Constants };
-//# sourceMappingURL=constants.js.map
